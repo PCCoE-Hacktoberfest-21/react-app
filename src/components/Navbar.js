703e85e69a4bd55a186de2514078fe8998672f37
ShamikG17 <shamikghadge.17@gmail.com>
2021-10-01 23:30:32 +0530

    Remove nav items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,60 +36,6 @@ const Navbar = () => {
         <NavLink to="/" className="navbar-logo" onClick={closeMobileMenu}>
           <span className="logo-text">Random Users</span>
         </NavLink>
-        <div className="menu-icon" onClick={handleClick}>
-          <i className={click ? "fas fa-times" : "fas fa-bars"} />
-        </div>
-        <ul className={click ? "nav-menu active" : "nav-menu"}>
-          <li
-            className="nav-item"
-            onMouseEnter={onMouseEnter}
-            onMouseLeave={onMouseLeave}
-          >
-            <NavLink
-              activeClassName="nav-links-active"
-              exact
-              to="/products"
-              className="nav-links"
-              onClick={closeMobileMenu}
-            >
-              Products <i className="fas fa-caret-down" />
-            </NavLink>
-            {/* {dropdown && <Dropdown />} */}
-          </li>
-          <li className="nav-item">
-            <NavLink
-              activeClassName="nav-links-active"
-              exact
-              to="/gallery"
-              className="nav-links"
-              onClick={closeMobileMenu}
-            >
-              Gallery
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              activeClassName="nav-links-active"
-              exact
-              to="/about"
-              className="nav-links"
-              onClick={closeMobileMenu}
-            >
-              About
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              activeClassName="nav-links-active"
-              exact
-              to="/contact-us"
-              className="nav-links"
-              onClick={closeMobileMenu}
-            >
-              Contact Us
-            </NavLink>
-          </li>
-        </ul>
       </nav>
     </>
   );
