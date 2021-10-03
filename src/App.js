327ca44becef826e3f02c31eb999971c02d17224
Padmanabh Kharat <70370670+Padmanabh82@users.noreply.github.com>
2021-10-03 15:49:41 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import Scroll_button from "./components/scroll-button";
 
 function App() {
   return (
     <>
       <Router>
-        <Navbar />
-        <Home />
-        <Footer/>
+        <div>
+          <Navbar />
+          <Home />
+          <Scroll_button />
+        </div>
+        <Footer />
       </Router>
     </>
   );
